39e5ffd8dceb2f7b950295d43d08b6144bbbeaf8
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-16 10:04:58 -0500

    feat(project): :bento: add asset logo-store.png

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,40 +3,45 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <a href="">
-          <div>
+    <nav className="w-full">
+      <div className="flex justify-between items-center gap-[30px] w-full px-[20px] h-[56px] bg-slate-500">
+        <div>
+          <a href="" className="flex">
+            <div>
+              <Image src="/images/logo-store.png" alt="logo-store" width={45} height={45}/>
+            </div>
+
+            <div>
+              <mark>Skiva</mark>.shop API
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <button type="button">
+            <div>
+              <div>
+                <Image src="" alt="" />
+              </div>
+              Search
+            </div>
+            <div>
+              <Image src="" alt="" />
+            </div>
+          </button>
+        </div>
+
+        <div className="flex">
+          <a href="">
+            i
             <Image src="" alt="" />
-          </div>
+          </a>
 
-          <div>
-            <mark>Skiva</mark>.shop API
-          </div>
-        </a>
-
-        <button type="button">
-          <div>
-          <div><Image src="" alt=""/></div>
-          Search
-          </div>
-          <div>
+          <button type="button">
+            o
             <Image src="" alt="" />
-          </div>
-
-        </button>
-
-
-      </div>
-
-      <div>
-        <a href="">
-          <Image src="" alt="" />
-        </a>
-        
-        <button type="button">
-          <Image src="" alt="" />
-        </button>
+          </button>
+        </div>
       </div>
     </nav>
   );
