8343d65de2347f89a96c9244c34d89bbf28823e8
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-16 10:43:46 -0500

    style(Navbar): :lipstick: change logo to store-logo.png, add styles for desktop design

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,21 +4,21 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className="w-full">
-      <div className="flex justify-between items-center gap-[30px] w-full px-[20px] h-[56px] bg-slate-500">
+      <div className="flex justify-between items-center gap-[30px] w-full px-[20px] h-[56px] bg-slate-purple">
         <div>
-          <a href="" className="flex">
+          <a href="" className="flex justify-center items-center gap-[15px]">
             <div>
-              <Image src="/images/logo-store.png" alt="logo-store" width={45} height={45}/>
+              <Image src="/images/store-logo.png" alt="store-logo" width={45} height={45}/>
             </div>
 
-            <div>
-              <mark>Skiva</mark>.shop API
+            <div className="text-[24px] text-lavender-gray">
+              <mark className="bg-transparent font-bold text-white">Skiva</mark>.shop API
             </div>
           </a>
         </div>
 
-        <div>
-          <button type="button">
+        <div className="grow flex justify-end">
+          <button type="button" className="w-full max-w-[300px] border border-white">
             <div>
               <div>
                 <Image src="" alt="" />
