464b215b6fe20035a9496029c0eea6dca361f92a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-22 16:54:46 -0500

    feat(project): :sparkles: add products route

---
+++ app/products/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import RightSidebar from '../components/RightSidebar'
+
+const page = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 order-1 w-full xl:max-w-[75%] ">
+        <div>
+          products
+        </div>
+      </main>
+      <RightSidebar />
+    </>
+  )
+}
+
+export default page
