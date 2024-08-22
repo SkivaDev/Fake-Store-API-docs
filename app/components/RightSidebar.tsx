611b0f5b0f919947982d68473b3323f8644cfefb
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-22 12:07:28 -0500

    style(RightSidebar): :sparkles: add styles and before before styles to li elements

--- app/components/RightSidebar.tsx
+++ app/components/RightSidebar.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 const RightSidebar = () => {
   return (
     <aside className='hidden xl:block order-2 relative w-full max-w-[25%]'>
-        <div className='fixed top-0 pt-[57px] bg-red-400'>
-          <div>
-            <ul>
-              <li>
-                <a href="">Controllers</a>
+        <div className='fixed top-0 pt-[57px] '>
+          <div className='pl-[30px]'>
+            <ul className='rightSidebar-submenu relative my-[16px] text-[14px]'>
+              <li className='submenu__element relative pl-[30px] mb-[6px] text-nav-purple rounded-[4px] hover:text-white'>
+                <a href="">Overview</a>
+              </li>
+              <li className='submenu__element relative pl-[30px] mb-[6px] text-nav-purple rounded-[4px] hover:text-white'>
+                <a href="">Get all products</a>
               </li>
             </ul>
           </div>
