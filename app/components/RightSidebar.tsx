4af3137d7cc7a32c0e340f824f6117b1d7778970
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-22 10:57:57 -0500

    feat(RightSidebar): :sparkles: add HTML structure

--- app/components/RightSidebar.tsx
+++ app/components/RightSidebar.tsx
@@ -4,7 +4,13 @@ const RightSidebar = () => {
   return (
     <aside className='hidden xl:block order-2 relative w-full max-w-[25%]'>
         <div className='fixed top-0 pt-[57px] bg-red-400'>
-          <p>ass</p>
+          <div>
+            <ul>
+              <li>
+                <a href="">Controllers</a>
+              </li>
+            </ul>
+          </div>
         </div>
     </aside>
   )
