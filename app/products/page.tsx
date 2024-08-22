e04d16dcf2b7212c76616237046f2761c85c1a1e
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-22 16:57:48 -0500

    feat(products): :sparkles: add HTML structure

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,7 +6,13 @@ const page = () => {
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24 order-1 w-full xl:max-w-[75%] ">
         <div>
-          products
+          <div>
+            <h1>Products</h1>
+          </div>
+        </div>
+        <div>
+            <div>main content</div>
+            <footer>footer</footer>
         </div>
       </main>
       <RightSidebar />
