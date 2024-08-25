f5b908d91a654fa3651dd4ebe03c87eb2b5a803d
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-24 22:26:30 -0500

    refactor(products): :art: format code

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import RightSidebar from '../components/RightSidebar'
+import React from "react";
+import RightSidebar from "../components/RightSidebar";
 
 const ProductsPage = () => {
   return (
     <>
       <main className="order-1 min-h-screen w-full xl:max-w-[75%] ">
-        <div className='p-[24px] w-full'>
-          <div className='ml-auto w-full max-w-[900px]'>
-            <h1 className='text-[42px] font-bold'>Products</h1>
+        <div className="p-[24px] w-full">
+          <div className="ml-auto w-full max-w-[900px]">
+            <h1 className="text-[42px] font-bold">Products</h1>
           </div>
         </div>
-        <div className='mt-[1px] p-[24px] w-full'>
-            <div className='ml-auto w-full max-w-[900px]'>main content</div>
-            <footer>footer</footer>
+        <div className="mt-[1px] p-[24px] w-full">
+          <div className="ml-auto w-full max-w-[900px]">main content</div>
+          <footer>footer</footer>
         </div>
       </main>
       <RightSidebar />
     </>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
